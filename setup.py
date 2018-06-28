dbe77c6a3132b635672de160172236bee7c93d28
Russ Garrett <russ@garrett.co.uk>
2018-06-28 11:47:20 +0100

    setup.py updates

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ import setuptools
 
 setuptools.setup(name='powerplan',
                  version='0.0.1',
-                 description='',
-                 long_description='',
+                 description='A library for computer-aided design of temporary power systems.',
+                 long_description='A library for computer-aided design of temporary power systems.',
                  author='Russ Garrett',
                  author_email='russ@garrett.co.uk',
-                 packages=['powerplan', 'powerplan.command'],
-                 install_requires=['networkx==2.1', 'pydotplus', 'click', 'pint'],
+                 packages=['powerplan'],
+                 install_requires=[
+                     'networkx==2.1',
+                     'pydotplus==2.0.2',
+                     'pint==0.8.1'],
                  python_requires=">=3.4",
                  license='GPL v3',
                  zip_safe=False)
