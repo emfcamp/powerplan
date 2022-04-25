5fc2d67190c32e0928b6bb65c59013e5e63d39fb
Russ Garrett <russ@garrett.co.uk>
2022-04-25 11:20:28 +0100

    Add CSV BOM export

--- powerplan/bom.py
+++ powerplan/bom.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, TextIO
+import csv
 from collections import defaultdict
 from jinja2 import Environment, PackageLoader, select_autoescape
 
@@ -31,3 +32,16 @@ def generate_bom_html(plan: Plan):
     nodes, edges = generate_bom(plan)
     template = env.get_template("bom.html")
     return template.render(nodes=nodes, edges=edges, plan=plan)
+
+
+def generate_bom_csvs(plan: Plan, distros_file: TextIO, cables_file: TextIO):
+    nodes, edges = generate_bom(plan)
+    distros_writer = csv.writer(distros_file)
+    distros_writer.writerow(["type", "part", "count"])
+    for node_type, used in nodes.items():
+        distros_writer.writerow([node_type[0], node_type[1], len(used)])
+
+    cables_writer = csv.writer(cables_file)
+    cables_writer.writerow(["I", "phases", "length", "count"])
+    for edge_type, used in edges.items():
+        cables_writer.writerow([edge_type[0], edge_type[1], edge_type[2], len(used)])
