3ef3de3591d0f62b461cc943acc3c4971340aa8c
Ben Hussey <ben@blip2.net>
2022-05-17 22:23:53 +0100

    updated to provide revised test selection logic - Test each adjustable RCD - Test the end of each three phase run - Test the distro fed from the longest submain on each grid - Test control position supply from each powercube

--- powerplan/test_schedules.py
+++ powerplan/test_schedules.py
@@ -10,29 +10,76 @@ env = Environment(
     loader=PackageLoader("powerplan", "templates"), autoescape=select_autoescape(["html", "xml"])
 )
 
+# FIXME: This probably shouldn't live here...
+ADJUSTABLE = ["Adjustable", "adjustable", "100-1000mA"]
+
 
 def generate_schedule(plan: Plan):
-    tests = []
-    for node in plan.nodes():
-        # Don't include generator nodes in test sheet
-        if node.get_spec()["type"] == "generator":
-            continue
+    tests = {}
 
-        # Test schedule entry for each RCD type per distro
-        output_types = {}
-        for output in node.get_spec()["outputs"]:
-            device_type = str(output["phases"]) + getattr(output, 'rcd', '')
-            output_types[device_type] = output
+    for grid in plan.grids():
+        tests[grid.name] = {}
 
-        for test in output_types:
-            output = output_types[test]
-            tests.append({
-                "type": type(node).__name__,
-                "name": node.name,
-                "phases": output["phases"],
-                "current": output["current"],
-                "rcd": output.get("rcd", ""),
-            })
+        longest = None
+
+        for a, b, data in grid.edges():
+            three_phase_output = False
+            if not longest:
+                longest = b
+
+            for c, c_data in b.outputs():
+                # Test each adjustable RCD
+                if c_data["rcd"] in ADJUSTABLE:
+                    print(c.name, "adjustable")
+                    tests[grid.name][c.name] = {
+                        "source": b,
+                        "node": c,
+                        "data": c_data,
+                    }
+
+                # Check if there are any used 3ph outputs
+                if c_data["phases"] == 3:
+                    three_phase_output = True
+
+            # Test the end of each three phase run
+            if data["phases"] == 3 and not three_phase_output:
+                print(b.name, "end of run")
+                tests[grid.name][b.name] = {
+                    "source": a,
+                    "node": b,
+                    "data": data,
+                }
+
+            # Test a circuit from the longest submain of each grid
+            # FIXME: can return inconsistent results for matching distances based on .outputs() sort order
+            if b.cable_length_from_source() > longest.cable_length_from_source():
+                longest = b
+
+            # Test control position supply from each powercube
+            if b.get_spec()["ref"] == "Powercube":
+                # FIXME: Better selection of output and add RCD to powercube spec
+                name = b.name + "-pc"
+                output = b.get_spec()["outputs"][0]
+                output["rcd"] = "30mA"
+                print(name, "powercube")
+                tests[grid.name][name] = {
+                    "description": "Control position power from " + b.name,
+                    "source": b,
+                    "data": output,
+                    "final": True,
+                }
+
+        # Test the distro further from the source
+        if longest:
+            print(b.name, "longest")
+            tests[grid.name][b.name] = {
+                "source": a,
+                "node": b,
+                "data": data,
+            }
+
+        # Sort alphabetically by key
+        tests[grid.name] = sorted(tests[grid.name].items())
 
     return tests
 
@@ -40,4 +87,4 @@ def generate_schedule(plan: Plan):
 def generate_schedule_html(plan: Plan):
     tests = generate_schedule(plan)
     template = env.get_template("test_schedules.html")
-    return template.render(tests=tests, plan=plan)
+    return template.render(tests=tests, plan=plan, adjustable=ADJUSTABLE)
